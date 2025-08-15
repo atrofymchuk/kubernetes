3df8544a447bf2237d2d9ff6a76c0f87fc2a4f7c
Andrii Trofymchuk <expert.it.andrey@gmail.com>
2025-08-15 11:41:24 +0300

    Added a test task

---
+++ test_task/app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+# Database connection details (replace with your actual details or use environment variables)
+DB_HOST = os.environ.get('DATABASE_HOST')
+DB_NAME = os.environ.get('DATABASE_NAME')
+DB_USER = os.environ.get('DATABASE_USER')
+DB_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+DB_PORT = "5432"
+
+@app.route('/')
+def index():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
+        )
+        #connection_status = "Connected successfully!"
+        cur = conn.cursor()
+        # Example: Execute a query
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        conn.close()
+    except Exception as e:
+        connection_status = f"Connection failed: {e}"
+    
+    return render_template('index.html', status=db_version)
+
+if __name__ == '__main__':
+    app.run(debug=True)
